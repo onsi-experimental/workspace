c093642fcf1f32c11874b04c862e0eaa25bbda21
Onsi Fakhouri <pair+onsi@pivotallabs.com>
2014-01-28 20:32:02 -0700

    update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+echo "Updating Submodules"
 git submodule update --init
 
-go install github.com/gocart/vito
+echo "Installing gocart"
+go install github.com/vito/gocart
 
+echo "Running gocart"
 gocart
 
+echo "Building etcd"
 mkdir -p $GOPATH/bin
 pushd $GOPATH/src/github.com/coreos/etcd
 ./build
 mv ./bin/etcd $GOPATH/bin/etcd
 popd
 
+echo "Installing ginkgo"
 go install github.com/onsi/ginkgo/ginkgo
